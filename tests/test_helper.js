429f745dcdc3abd0ce0bf92010ee4fd1f3bcfeb9
oolongraccoon <133898805+oolongraccoon@users.noreply.github.com>
2024-03-01 04:51:04 +0800

    part4

--- tests/test_helper.js
+++ tests/test_helper.js
@@ -28,14 +28,6 @@ const initialBlogs = [
   }
 ]
 
-// const nonExistingId = async () => {
-//   const note = new Note({ content: 'willremovethissoon' })
-//   await note.save()
-//   await note.deleteOne()
-
-//   return note._id.toString()
-// }
-
 const blogsInDb = async () => {
   const blogs = await Blog.find({})
   return blogs.map(blog => blog.toJSON())
